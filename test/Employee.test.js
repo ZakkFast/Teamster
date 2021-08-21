bff97e33e43a32f18d996c64c6a67b0fc0a11585
Zakk Fast <zakkfastpro@gmail.com>
2021-08-20 22:20:59 -0700

    changed test conditions to make more sense

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -2,12 +2,6 @@ const NumberPrompt = require('inquirer/lib/prompts/number')
 const { TestWatcher } = require('jest')
 const Employee = require('../lib/Employee')
 
-test('Testing for creation of an Employee Object', ()=>{
-    const employee = new Employee('John Bonham', 77, 'johnbonham@no.com')
-    expect(employee.name).toEqual(expect.any(String))
-    expect(employee.id).toEqual(expect.any(Number))
-    expect(employee.email).toEqual(expect.any(String))
-})
 test('Testing that name is returned correctly', ()=>{
     const employee = new Employee('John Bonham', 77, 'johnbonham@no.com')
     expect(employee.getName()).toEqual(expect.any(String))
