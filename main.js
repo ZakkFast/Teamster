d47abfee3c184fa97cccc59c4810a0df8515bc51
Zakk Fast <zakkfastpro@gmail.com>
2021-08-21 17:51:54 -0700

    finished inquirer prompts and loop. Pushed to array, awaiting html

--- main.js
+++ main.js
@@ -68,5 +68,100 @@ const createManager = () => {
         console.log('Manager successfully stored.')
     })
 }
+const createEmployee = () => {
+    console.log('Please answer the following questions.')
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'role',
+            message: 'What is this employees role?',
+            choices: ['Intern', 'Engineer']
+        },
+        {
+            name: 'name',
+            message: 'What is the Employee name?',
+            validate: nameInput =>{
+                if(nameInput){
+                    return true
+                } else {
+                    console.log('You must enter a name to continue')
+                    return false
+                }
+            }
+        },
+        {
+            name: 'id',
+            message: 'What is the Employees id number?',
+            validate: nameInput => {
+                if(nameInput){
+                    return true
+                } else {
+                    console.log('You must enter a name to continue')
+                    return false
+                }
+            }
+        },
+        {
+            name: 'email',
+            message: 'What is the employees email adress?',
+            validate: nameInput => {
+                if(nameInput){
+                    return true
+                } else{
+                    console.log('You must enter an email to continue')
+                    return false
+                }
+            }
+        },
+        {
+            name: 'school',
+            message: 'What school does the intern attend?',
+            when: (input) => input.role === 'Intern',
+            validate: nameInput => {
+                if (nameInput){
+                    return true
+                } else {
+                    console.log('You must enter the school to continue')
+                }
+            },   
+        },
+        {
+            name: 'github',
+            message: 'What is the Engineers GitHub username?',
+            when: (input) => input.role === 'Engineer',
+            validate: nameInput => {
+                if(nameInput){
+                    return true
+                } else {
+                    console.log('You must enter the GitHub username to continue')
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmCreateEmployee',
+            message: 'Are there more employess to add?',
+            defualt: false
+        }
+    ])
+    .then(employeeData => {
+        let {name, id, email, role, github, school, confirmCreateEmployee} = employeeData
+        let employee
+        if(role === 'Engineer'){
+            employee = new Engineer(name, id, email, github)
+            console.log(employee)
+        } else if(role === 'Intern'){
+            employee = new Intern(name, id, email, school)
+            console.log(employee)
+        }
+        teamMembersArray.push(employee)
+        if(confirmCreateEmployee){
+            return createEmployee(teamMembersArray)
+        } else {
+            return teamMembersArray
+        }
+    })
+}
 
 createManager()
+    .then(createEmployee)
