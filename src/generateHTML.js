4ad708056f103c2423d1e4294f0eb30ada0c0002
Zakk Fast <zakkfastpro@gmail.com>
2021-08-21 18:39:15 -0700

    added "cards" for roles. Ready for import

---
+++ src/generateHTML.js
@@ -0,0 +1,51 @@
+const createManagerHtml = function(manager){
+    return `
+    <div class='col-4 mt-3>
+        <div class='card h-100'>
+            <div class ="card-header">
+                <h2>${manager.name}</h2>
+                <h3>Manager</h3>
+            </div>
+            <div class='card-body'>
+                <p>ID: ${manager.id}</p>
+                <p>Email: ${manager.email}</p>
+                <p>Office: ${manager.office}</p>
+            </div>
+        </div>
+    </div>
+    `
+}
+const createEngineerHtml = function(engineer){
+    return `
+    <div class='col-4 mt-3>
+        <div class='card h-100'>
+            <div class ="card-header">
+                <h2>${engineer.name}</h2>
+                <h3>engineer</h3>
+            </div>
+            <div class='card-body'>
+                <p>ID: ${engineer.id}</p>
+                <p>Email: ${engineer.email}</p>
+                <p>Github: ${engineer.github}</p>
+            </div>
+        </div>
+    </div>
+    `
+}
+const createInternHtml = function(intern){
+    return `
+    <div class='col-4 mt-3>
+        <div class='card h-100'>
+            <div class ="card-header">
+                <h2>${intern.name}</h2>
+                <h3>intern</h3>
+            </div>
+            <div class='card-body'>
+                <p>ID: ${intern.id}</p>
+                <p>Email: ${intern.email}</p>
+                <p>School: ${intern.school}</p>
+            </div>
+        </div>
+    </div>
+    `
+}
